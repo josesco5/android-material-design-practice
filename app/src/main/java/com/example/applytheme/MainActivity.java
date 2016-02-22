5b9c4494ebac327bdd8298835c7a3533129e796f
José Escorche <josesco5@gmail.com>
2016-02-21 22:42:38 -0430

    Set standalone toolbar

--- app/src/main/java/com/example/applytheme/MainActivity.java
+++ app/src/main/java/com/example/applytheme/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.applytheme;
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -18,59 +16,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle("Welcome!");
+        toolbar.setTitle("Welcome!");
         toolbar.setSubtitle("Folks!");
 
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                String msg = "";
 
-    }
+                switch (id) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                    case R.id.action_discard:
+                        msg = "Delete";
+                        break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        String msg = "";
+                    case R.id.action_search:
+                        msg = "Search";
+                        break;
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+                    case R.id.action_settings:
+                        msg = "Settings";
+                        break;
 
-        switch (id) {
+                    case R.id.action_edit:
+                        msg = "Edit";
+                        break;
 
-            case R.id.action_discard:
-                msg = "Delete";
-                break;
+                    case R.id.action_exit:
+                        msg = "Exit";
+                        break;
+                }
 
-            case R.id.action_search:
-                msg = "Search";
-                break;
+                Toast.makeText(MainActivity.this, msg + " clicked!", Toast.LENGTH_SHORT).show();
 
-            case R.id.action_settings:
-                msg = "Settings";
-                break;
-
-            case R.id.action_edit:
-                msg = "Edit";
-                break;
-
-            case R.id.action_exit:
-                msg = "Exit";
-                break;
-        }
-
-        Toast.makeText(this, msg + " clicked!", Toast.LENGTH_SHORT).show();
-
-        return super.onOptionsItemSelected(item);
+                return true;
+            }
+        });
     }
 }
